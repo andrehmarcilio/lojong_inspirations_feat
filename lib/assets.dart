6e24a99758ce224e6d52b27819c4c9623d188360
Andre Marcilio <andremarcilio@jera.com.br>
2024-01-13 10:06:26 -0300

    [NEW] added app assets

---
+++ lib/assets.dart
@@ -0,0 +1,16 @@
+abstract class ImagePaths {
+  static const _imagesBasePath = 'assets/images';
+
+  static String lotus = '$_imagesBasePath/lotus.svg';
+  static String mountains = '$_imagesBasePath/mountains.svg';
+}
+
+abstract class IconPaths {
+  static const _iconsBasePath = 'assets/icons';
+
+  static String icBack = '$_iconsBasePath/ic_back_button.svg';
+  static String icPlayWhite = '$_iconsBasePath/ic_play_white.svg';
+  static String icPlayBlue = '$_iconsBasePath/ic_play_blue.svg';
+  static String icShareGrey = '$_iconsBasePath/ic_share_gray.svg';
+  static String icShareWhite = '$_iconsBasePath/ic_share_white.svg';
+}
