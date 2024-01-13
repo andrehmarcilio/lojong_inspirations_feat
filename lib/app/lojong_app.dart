6e05b54d2effff5866ad0b64c33e344afb1cec66
Andre Marcilio <andremarcilio@jera.com.br>
2024-01-13 10:25:27 -0300

    [NEW] added l10n strings

--- lib/app/lojong_app.dart
+++ lib/app/lojong_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:lojong_test_app/app/theme/theme.dart';
+import 'package:lojong_test_app/utils/extensions/context_extensions.dart';
 
 class LojongApp extends StatelessWidget {
   const LojongApp({super.key});
@@ -9,6 +11,9 @@ class LojongApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
+      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      onGenerateTitle: (context) => context.l10n.appTitle,
     );
   }
 }
