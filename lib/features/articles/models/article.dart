ba27df97e1b965a84a92fd2ca7f85a09ea3c9859
Andre Marcilio <andremarcilio@jera.com.br>
2024-01-14 17:26:59 -0300

    [NEW] added article entity

---
+++ lib/features/articles/models/article.dart
@@ -0,0 +1,13 @@
+class Article {
+  final int id;
+  final String title;
+  final String text;
+  final String imageUrl;
+
+  Article({
+    required this.id,
+    required this.title,
+    required this.text,
+    required this.imageUrl,
+  });
+}
