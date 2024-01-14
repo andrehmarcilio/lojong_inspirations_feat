e25bf3ace51b4b010523005f5abc76a8f94b7067
Andre Marcilio <andremarcilio@jera.com.br>
2024-01-14 17:30:27 -0300

    [NEW] created content section widget

--- lib/features/videos/components/video_item_widget.dart
+++ lib/features/videos/components/video_item_widget.dart
@@ -1,10 +1,8 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../assets.dart';
-import '../../../common/components/share_button.dart';
+import '../../../common/components/content_section_widget.dart';
 import '../../../common/components/svg_widget.dart';
-import '../../../utils/style_helpers.dart';
 import '../models/video.dart';
 
 class VideoItemWidget extends StatelessWidget {
@@ -17,51 +15,15 @@ class VideoItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        const SizedBox(height: 12),
-        Text(
-          video.name,
-          style: AppFonts(context).titleMedium,
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 12),
-        Material(
-          elevation: 1.5,
-          borderRadius: BorderRadius.circular(20),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Stack(
-                fit: StackFit.expand,
-                children: [
-                  CachedNetworkImage(imageUrl: video.imageUrl),
-                  Center(
-                    child: SvgWidget(
-                      width: 52,
-                      height: 52,
-                      assetPath: IconPaths.icPlayWhite,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(height: 12),
-        Text(
-          video.description,
-          style: AppFonts(context).bodyMedium,
-          textAlign: TextAlign.center,
-          maxLines: 3,
-          overflow: TextOverflow.ellipsis,
-        ),
-        const SizedBox(height: 12),
-        const Center(child: ShareButton()),
-        const SizedBox(height: 16),
-      ],
+    return ContentSectionWidget(
+      title: video.name,
+      imageUrl: video.imageUrl,
+      description: video.description,
+      foregroundImageIcon: SvgWidget(
+        width: 52,
+        height: 52,
+        assetPath: IconPaths.icPlayWhite,
+      ),
     );
   }
 }
