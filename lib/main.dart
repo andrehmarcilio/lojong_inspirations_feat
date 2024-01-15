bf807dd296e583b8ad9c2d82e0ea4caea7e93363
Andre Marcilio <andremarcilio@jera.com.br>
2024-01-15 19:58:14 -0300

    [FIX] lock device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app/lojong_app.dart';
 import 'utils/service_locator/service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDependencies();
+  await Future.wait([
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]),
+    initializeDependencies()
+  ]);
   runApp(const LojongApp());
 }
