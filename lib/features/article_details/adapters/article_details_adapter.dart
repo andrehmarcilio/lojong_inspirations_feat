cc74d4e9e7ca8da27cfd3cd2f6983344b08c2115
Andre Marcilio <andremarcilio@jera.com.br>
2024-01-15 07:29:57 -0300

    [NEW] implemented article_details model

---
+++ lib/features/article_details/adapters/article_details_adapter.dart
@@ -0,0 +1,13 @@
+import '../models/article_details.dart';
+
+abstract class ArticleDetailsAdapter {
+  static ArticleDetails fromMap(Map map) => ArticleDetails(
+        id: map['id'],
+        text: map['text'],
+        title: map['title'],
+        imageUrl: map['image_url'],
+        authorName: map['author_name'],
+        authorDescription: map['author_description'],
+        htmlContent: map['full_text'],
+      );
+}
